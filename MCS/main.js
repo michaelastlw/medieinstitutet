a0743305f3159735a2aaacd846a1c5d54aad3209
Daniel Westerling <daniel@westerling.io>
2023-10-06 18:24:58 +0200

    Daniels svar på frågorna

--- MCS/main.js
+++ MCS/main.js
@@ -14,20 +14,44 @@ function startTime() {
     let s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
+    console.log(s);
     // Eftersom h inte kopplas till funktionen checkTime tänker vi att det inte kommer visas några nollor innan klockan 10.
     // FRÅGA DANIEL om ovan
+    /*
+     Daniels svar: 
+     Det är helt korrekt, vill ni inte ha 0 framför!
+    */
+
+    /*
+     Daniels svar:
+     Ni har dubbla + i slutet, tror det är det som förstör sekundrarna!
+    */
     document.getElementById('clock').innerHTML = h + ":" + m + ":" +  + s;
     setTimeout(startTime, 1000);
     // innerHTML - FRÅGA DANIEL
+
+    /*
+     Daniels svar:
+     innerHTML fungerar bra, det som innerHTML gör är att ersätta all HTML inuti #clock, 
+     medan innerText bara ersätter texten och sparar HTML.
+    */
  }
 
  function checkTime(i) {
-    if (i < 10) {i = "0" + i};
+    if (i < 10) {
+        console.log(i);
+        i = "0" + i;
+        console.log(i);
+    };
+
     return i;
 }
       // FRÅGA DANIEL: funktionen fungerar för minutrar men ej sekunder, vi kan ej hitta fel i koden. hjälp.
     // lägger till nollor till alla nummer under 10, för sekunder och minuter
-    
+    /*
+        Daniels svar: 
+        Se mitt svar på rad 26 och rad 27
+    */
     function validateForm() {
         let x = document.forms["myForm"]["fname"].value;
         if (x == "") {
